0d997351445278afa65014f9e564fb353227ed85
topsail_cbd <1911352135@qq.com>
2022-12-19 18:50:09 +0800

    121918 add mapreduce misragreis

--- map_reduce_v1.py
+++ map_reduce_v1.py
@@ -17,42 +17,59 @@ import time
 import os
 from multiprocessing import Process,Manager,Pool
 
-def Map_v0(sentences,lis):  #Map函数进行分词并存储到列表
+def Map_v1(sentences,k,lis):  #Map函数进行分词并存储到列表
     print('process %d start'%os.getpid())
+    dic = {}
+    
     for snt_idx,snt in enumerate(sentences):
         words = word_tokenize(snt['text'])
-        
         # print('process %d working on sentence %d'%(os.getpid(),snt['id']))
         
         for wd in words:
-            if not is_punctuation(wd):
-                lis.append((wd.lower(),1))
+            if is_punctuation(wd):
+                continue
+            
+            if len(dic.keys())<k-1 or wd in dic.keys():
+                dic[wd] = dic.get(wd,0)+1
+            else:
+                del_keys = []
+                
+                for key in dic.keys():
+                    if dic[key] == 1:
+                        del_keys.append(key)
+                    else:
+                        dic[key] = dic[key] - 1
+                        
+                for key in del_keys:
+                    del dic[key]
                 
         if snt_idx % 2000 == 0:
             print('process %d has done %d sentences'%(os.getpid(),snt_idx))
+    lis.append(dic)
     #print(len(lis))
 
-def Reduce_v0(lis):  #Reduce函数将结果汇总到字典中
+def Reduce_v1(lis):  #Reduce函数将结果汇总到字典中
     # print('time2 = %f'%(time.time()-start_time))  #测试Map函数总耗时（分词总耗时）
     dic = {}
-    for k,v in tqdm(lis):
-        dic[k] = dic.get(k,0)+1
-        #print(dic)
+    
+    for d in lis:
+        for k,v, in tqdm(d.items()):
+            dic[k] = dic.get(k,0)+v
+    
     dic_order=sorted(dic.items(),key=lambda x:x[1],reverse=True)  #字典降序排序
-    with open('log_mr_v0.txt','a',encoding='utf-8') as file:
+    with open('log_mr_v1.txt','a',encoding='utf-8') as file:
         for k,v in dic_order:
             file.write(k+':'+str(v)+'\n')  #将结果写入文件
 
-def Map_Reduce_v0(dataset,item_per_worker=10000,max_iter=100000):
+def Map_Reduce_v1(dataset,showed_k=100,k=1000,item_per_worker=10000,max_iter=100000):
     start_time = time.time()
     
-    log_file = './log_mr_v0.txt'
+    log_file = './log_mr_v1.txt'
     with open(log_file,'w') as f:
         f.truncate()
 
     plist = []
-    # 用Pool改写一下，别用list of Process了
-    # pool = Pool(num_workers)
+
     m = Manager()
     managed_list = m.list([])
     
@@ -60,25 +77,24 @@ def Map_Reduce_v0(dataset,item_per_worker=10000,max_iter=100000):
     
     for i in range(max_iter//item_per_worker):   #创建进程
         sample = dataset.take(item_per_worker)
-        p = Process(target=Map_v0,args=(sample,managed_list))
+        p = Process(target=Map_v1,args=(sample,k,managed_list))
         plist.append(p)
-        # pool.apply_async(Map_v0,args=(sample,managed_list))
+
         dataset = dataset.skip(item_per_worker)
         print('time (Initialize process %d) = %f'%(i,time.time()-start_time))
-    # pool.close()
-    # pool.join()  
+
     for p in plist:
         p.start()  #启动进程
     for p in plist:
         p.join()  #阻滞主进程
     print('time2 (Map) = %f'%(time.time()-start_time))  #测试Map函数总耗时
     
-    Reduce_v0(managed_list) #当Map进程全部完成之后Reduce进行结果归约
+    Reduce_v1(managed_list) #当Map进程全部完成之后Reduce进行结果归约
     
     print('time3 (Reduce) = %f'%(time.time() - start_time))   #测试总用时
     
 if __name__ == '__main__': 
-    # Map_Reduce_v0: 2h22m23s
+    # Map_Reduce_v1(misra-greis multiprocess)
     # Map: 4522s
     # Reduce: 3979s
-    Map_Reduce_v0(shuffled_dataset)
+    Map_Reduce_v1(shuffled_dataset)
--- utils.py
+++ utils.py
@@ -1,17 +1,18 @@
 import os
 from tqdm import tqdm
 
-frequency = {}
+def correct_capitalization(file:str):
+    frequency = {}
 
-with open('log_mr_v0.txt','r',encoding='utf-8') as f:
-    for line in tqdm(f):
-        word,freq = line.split(':')
-        word = word.lower()
-        freq = int(freq)
-        frequency[word] = frequency.get(word,0)+freq
-        
-frequency = sorted(frequency.items(),key=lambda x:x[1],reverse=True)
-        
-with open('log_mr_v0_new.txt','a',encoding='utf-8') as file:
-    for k,v in frequency:
-        file.write(k+':'+str(v)+'\n')  #将结果写入文件
+    with open(file,'r',encoding='utf-8') as f:
+        for line in tqdm(f):
+            word,freq = line.split(':')
+            word = word.lower()
+            freq = int(freq)
+            frequency[word] = frequency.get(word,0)+freq
+            
+    frequency = sorted(frequency.items(),key=lambda x:x[1],reverse=True)
+            
+    with open(file.split('.')+'_new.txt','a',encoding='utf-8') as file:
+        for k,v in frequency:
+            file.write(k+':'+str(v)+'\n')  #将结果写入文件
